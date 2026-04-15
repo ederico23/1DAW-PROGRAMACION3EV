3e8f41f92abf49087ec3ae0a94783fdbf2a78800
eder23 <edergracia006@gmail.com>
2026-04-15 15:40:26 +0200

    .

--- InterfacesGraficas/src/ejercicios/Ejercicio11.java
+++ InterfacesGraficas/src/ejercicios/Ejercicio11.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 
 public class Ejercicio11 extends JFrame {
@@ -21,8 +22,8 @@ public class Ejercicio11 extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtPalabraSpain;
-	private String[] palabrasEspañol= {};
-	private String[] palabrasIngles= {};
+	private String[] palabrasEspañol= {"Perro", "Gato", "Casa", "Coche", "Árbol", "Libro", "Mesa", "Silla", "Sol", "Luna", "Agua", "Comida", "Manzana", "Niño", "Niña", "Feliz", "Grande", "Pequeño", "Correr", "Comer"};
+	private String[] palabrasIngles= {"Dog", "Cat", "House", "Car", "Tree", "Book", "Table", "Chair", "Sun", "Moon", "Water", "Food", "Apple", "Boy", "Girl", "Happy", "Big", "Small", "Run", "Eat"};
 
 	/**
 	 * Create the frame.
@@ -34,29 +35,70 @@ public class Ejercicio11 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		txtPalabraSpain = new JTextField();
 		txtPalabraSpain.setBounds(67, 40, 86, 20);
 		contentPane.add(txtPalabraSpain);
 		txtPalabraSpain.setColumns(10);
-		
+
 		JLabel lblMensaje = new JLabel("Palabra a traducir:");
 		lblMensaje.setBounds(67, 11, 112, 14);
 		contentPane.add(lblMensaje);
-		
+
 		JLabel lblTraducida = new JLabel("");
 		lblTraducida.setBounds(214, 103, 180, 14);
 		contentPane.add(lblTraducida);
-		
+
 		JButton btnTraductor = new JButton("TRADUCIR");
 		btnTraductor.setBounds(67, 99, 89, 23);
 		contentPane.add(btnTraductor);
 
-		
+
 		//LISTENERS
 		btnTraductor.addActionListener(e->{
 			String palabraATraducir = txtPalabraSpain.getText();
-			
+			boolean encontrada = false;
+			try {
+
+				if(palabraATraducir.isEmpty()) {
+					JOptionPane.showMessageDialog(
+							this,
+							"Introduce una palabra",
+							"Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					
+					for(int i = 0; i < palabrasEspañol.length; i++) {
+						if(palabraATraducir.equalsIgnoreCase(palabrasEspañol[i])) {
+							lblTraducida.setText(palabrasIngles[i]);
+							encontrada = true;
+							break;
+						}//fin ifelse palabras iguales
+					}//fin for palabrasEspañol
+					
+					if(!encontrada) {
+						JOptionPane.showMessageDialog(
+								this, 
+								"Palabra no encontrada en el diccionario", 
+								"Palabra no encontrada",
+								JOptionPane.WARNING_MESSAGE);
+						lblTraducida.setText("");
+					}//fin if no encontrada
+					
+				} //fin ifelse vacio
+
+
+			}catch (NumberFormatException ex) {
+
+				JOptionPane.showMessageDialog(
+						this,
+						"Introduce una palabra valida",
+						"Error",
+						JOptionPane.ERROR_MESSAGE);
+
+			}//fin trycatch
+
+
 		});//fin btnTraductor() listener
 	}//fin contructor
 }//fin class
