05cd4cfd09d239d63b5548e4c29d15298c9148fc
Eder Gracia <edergracia006@gmail.com>
2026-04-28 15:41:22 +0200

    .

--- AccesoDatosDAO2/src/dao/UsuarioDAO.java
+++ AccesoDatosDAO2/src/dao/UsuarioDAO.java
@@ -26,20 +26,48 @@ public class UsuarioDAO {
 	public List<Usuario> listar() throws Exception{
 		List<Usuario> lista = new ArrayList<>();
 		String sql = "SELECT * FROM USUARIOS";
-		
+
 		try (Connection conex = Database.getConnection();
 				PreparedStatement pstmt = conex.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery()){
-			
+
 			while(rs.next()) {
 				lista.add(new Usuario(
 						rs.getInt("id"),
 						rs.getString("nombre"),
 						rs.getInt("edad")));
 			}
-			
+
 			return lista;
-			
+
 		}//fin try
 	}//fin listar()
+
+	public void actualizar(Usuario u) throws Exception {
+
+		String sql = "UPDATE USUARIOS SET NOMBRE=?, EDAD=? WHERE ID=?";
+		
+		try (Connection conex = Database.getConnection();
+				PreparedStatement pstmt = conex.prepareStatement(sql)){
+			
+			pstmt.setString(1, u.getNombre());
+			pstmt.setInt(2, u.getEdad());
+			pstmt.setInt(3, u.getId());
+			pstmt.executeUpdate();
+				
+		}//fin try
+
+	} //fin actualizar()
+	
+	
+	public void eliminar(Usuario u) throws Exception {
+		String sql = "DELETE DROP USUARIOS WHERE ID=?";
+		
+		try (Connection conex = Database.getConnection();
+				PreparedStatement pstmt = conex.prepareStatement(sql)){
+			
+			pstmt.setInt(1, u.getId());
+			pstmt.executeUpdate();
+		}//fin try
+	} //fin eliminar()
 }//fin class
